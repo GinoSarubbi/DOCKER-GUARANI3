a2eda4d4b440de8ee89a0e4dd5abb796a754771f
gsarubbi <g.sarubbi.17@etec.uba.ar>
2023-08-30 16:46:13 -0300

    cambio nombre de script

--- install-G3.sh
+++ install-G3.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
-if [ -f ".env" ]; then # scrip executed from repository root
+if [ -f ".env" ]; then
     source .env
 fi
 
-echo "-- Ejecutando 'composer install'"
-docker exec -i $APACHE_CONTAINER composer install --no-interaction --optimize-autoloader
-echo "-- 'composer install' finalizado"
 
-echo "-- Instalando framework toba"
+animate_loading() {
+    local chars="/-\|"
+    for (( i=0; i<5; i++ )); do
+        echo -ne "\\e[1m$1 [${chars:$i:1}]\\e[0m\r"
+        sleep 0.2
+    done
+    echo -e "\\e[1m$1 [Done]\\e[0m"
+    sleep 1.5 
+}
+
+
+animate_loading "-- Ejecutando 'composer install'"
+docker exec -i $APACHE_CONTAINER composer install --no-interaction --optimize-autoloader
+
+
+animate_loading "-- Instalando framework toba"
 docker exec -it $APACHE_CONTAINER bash -c "cd bin && export TOBA_INSTANCIA=desarrollo"
 docker exec -it $APACHE_CONTAINER bash -c "cd bin && export TOBA_INSTALACION_DIR=/instalacion"
 docker exec -it $APACHE_CONTAINER bash -c "cd bin && ./toba instalacion instalar -d /usr/local/proyectos/guarani"
-echo "-- Framework toba instalado"
 
-echo "-- Configurando permisos"
+animate_loading "-- Configurando permisos"
 docker exec -it $APACHE_CONTAINER bash -c "chown -R root:www-data /usr/local/proyectos/guarani/www"
 docker exec -it $APACHE_CONTAINER bash -c "chown -R root:www-data /usr/local/proyectos/guarani/temp"
 docker exec -it $APACHE_CONTAINER bash -c "chown -R root:www-data /usr/local/proyectos/guarani/instalacion"
@@ -23,27 +34,32 @@ docker exec -it $APACHE_CONTAINER bash -c "chown -R root:www-data /usr/local/pro
 docker exec -it $APACHE_CONTAINER bash -c "chown -R root:www-data www temp instalacion vendor/siu-toba/framework/www vendor/siu-toba/framework/temp"
 docker exec -it $APACHE_CONTAINER bash -c "chmod 775 -R www temp instalacion vendor/siu-toba/framework/www vendor/siu-toba/framework/temp"
 
-echo "-- Permisos configurados"
 
-echo "-- Levantando configuracion de sitio"
+animate_loading "-- Levantando configuracion de sitio"
 docker exec -it $APACHE_CONTAINER bash -c "ln -s instalacion/toba.conf /etc/apache2/sites-available/toba_3_3.conf"
 docker exec -it $APACHE_CONTAINER bash -c "service apache2 reload"
-echo "-- Configuracion ok"
 
 
-echo "-- Aplicando el fop"
+animate_loading "-- Aplicando el fop"
 docker exec -it $APACHE_CONTAINER bash -c "echo '[xslfo]' >> ./instalacion/instalacion.ini"
 docker exec -it $APACHE_CONTAINER bash -c "echo 'fop=/usr/local/proyecto/guarani/php/3ros/fop/fop' >> ./instalacion/instalacion.ini"
-echo "aplicado el fop"
 
-echo "-- Cargando proyecto guarani"
+animate_loading "-- Cargando proyecto guarani"
 docker exec -it $APACHE_CONTAINER bash -c "cd bin && ./guarani cargar -d /usr/local/proyectos/guarani"
-echo "-- Proyecto guarani cargado"
 
-echo "-- Apache2 restart"
+animate_loading "-- Apache2 restart"
 docker exec -it $APACHE_CONTAINER bash -c "service apache2 reload"
-echo "-- Apache2 restart ok"
 
 
-echo "-- Instalando proyecto guarani"
+animate_loading "-- Proyecto guarani instalado"
 docker exec -it $APACHE_CONTAINER bash -c "cd bin && ./guarani instalar"
+
+
+animate_loading "-- Dando permisos nuevamente"
+docker exec -it $APACHE_CONTAINER bash -c "chown -R root:www-data /usr/local/proyectos/guarani/www"
+docker exec -it $APACHE_CONTAINER bash -c "chown -R root:www-data /usr/local/proyectos/guarani/temp"
+docker exec -it $APACHE_CONTAINER bash -c "chown -R root:www-data /usr/local/proyectos/guarani/instalacion"
+docker exec -it $APACHE_CONTAINER bash -c "chown -R root:www-data /usr/local/proyectos/guarani/vendor/siu-toba/framework/www"
+docker exec -it $APACHE_CONTAINER bash -c "chown -R root:www-data /usr/local/proyectos/guarani/vendor/siu-toba/framework/temp"
+docker exec -it $APACHE_CONTAINER bash -c "chown -R root:www-data www temp instalacion vendor/siu-toba/framework/www vendor/siu-toba/framework/temp"
+docker exec -it $APACHE_CONTAINER bash -c "chmod 775 -R www temp instalacion vendor/siu-toba/framework/www vendor/siu-toba/framework/temp"
